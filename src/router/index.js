184caaf253919036e57f5c524ae488b8830f228b
thomascassidyzm <thomas.cassidy+zm@gmail.com>
2025-07-20 00:39:42 +0100

    Updated apml.dev site to include the ADE docs

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
+import SpecificationPage from '../pages/SpecificationPage.vue'
+import PlaygroundPage from '../pages/PlaygroundPage.vue'
+import ExamplesPage from '../pages/ExamplesPage.vue'
+import ToolsPage from '../pages/ToolsPage.vue'
+import CommunityPage from '../pages/CommunityPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomePage,
-    meta: { showInNav: true }
+    meta: { showInNav: true, title: 'Home' }
+  },
+  {
+    path: '/specification',
+    name: 'Specification',
+    component: SpecificationPage,
+    meta: { showInNav: true, title: 'Specification' }
+  },
+  {
+    path: '/playground',
+    name: 'Playground',
+    component: PlaygroundPage,
+    meta: { showInNav: true, title: 'Playground' }
+  },
+  {
+    path: '/examples',
+    name: 'Examples',
+    component: ExamplesPage,
+    meta: { showInNav: true, title: 'Examples' }
+  },
+  {
+    path: '/tools',
+    name: 'Tools',
+    component: ToolsPage,
+    meta: { showInNav: true, title: 'Tools' }
+  },
+  {
+    path: '/community',
+    name: 'Community',
+    component: CommunityPage,
+    meta: { showInNav: true, title: 'Community' }
   }
 ]
 
