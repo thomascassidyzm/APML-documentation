af036e2f91c9a023d2b6b7f40d4c53c75a40c842
thomascassidyzm <thomas.cassidy+zm@gmail.com>
2025-07-21 15:28:19 +0100

    Updated code scrolling block

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -37,35 +37,105 @@
   <span class="property">email</span><span class="punctuation">:</span> <span class="type">email</span> <span class="modifier">required unique</span>
   <span class="property">profile_photo</span><span class="punctuation">:</span> <span class="type">image_url</span> <span class="modifier">optional</span>
   <span class="property">auth_provider</span><span class="punctuation">:</span> <span class="enum">google | facebook | email</span> <span class="modifier">required</span>
+  <span class="property">join_date</span><span class="punctuation">:</span> <span class="type">timestamp</span> <span class="modifier">auto</span>
   <span class="property">total_reviews</span><span class="punctuation">:</span> <span class="type">number</span> <span class="modifier">default</span> <span class="number">0</span>
+  <span class="property">onboarding_completed</span><span class="punctuation">:</span> <span class="type">boolean</span> <span class="modifier">default</span> <span class="number">false</span>
 
 <span class="keyword">data</span> <span class="type">City</span><span class="punctuation">:</span>
+  <span class="property">id</span><span class="punctuation">:</span> <span class="type">unique_id</span>
   <span class="property">name</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">required</span>
   <span class="property">country</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">required</span>
+  <span class="property">state_province</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">optional</span>
   <span class="property">added_by</span><span class="punctuation">:</span> <span class="type">User</span> <span class="modifier">required</span>
+  <span class="property">wikipedia_summary</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">optional</span>
   <span class="property">average_rating</span><span class="punctuation">:</span> <span class="type">number</span> <span class="modifier">default</span> <span class="number">0</span>
   <span class="property">total_reviews</span><span class="punctuation">:</span> <span class="type">number</span> <span class="modifier">default</span> <span class="number">0</span>
+  <span class="property">created_date</span><span class="punctuation">:</span> <span class="type">timestamp</span> <span class="modifier">auto</span>
 
 <span class="keyword">data</span> <span class="type">Review</span><span class="punctuation">:</span>
+  <span class="property">id</span><span class="punctuation">:</span> <span class="type">unique_id</span>
   <span class="property">city_id</span><span class="punctuation">:</span> <span class="type">City</span> <span class="modifier">required</span>
   <span class="property">user_id</span><span class="punctuation">:</span> <span class="type">User</span> <span class="modifier">required</span>
   <span class="property">rating</span><span class="punctuation">:</span> <span class="enum">1 | 2 | 3 | 4 | 5</span> <span class="modifier">required</span>
   <span class="property">title</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">required</span>
   <span class="property">review_text</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">required</span>
+  <span class="property">lived_there</span><span class="punctuation">:</span> <span class="type">boolean</span> <span class="modifier">default</span> <span class="number">false</span>
+  <span class="property">visited_duration</span><span class="punctuation">:</span> <span class="enum">days | weeks | months | years</span> <span class="modifier">optional</span>
+  <span class="property">review_date</span><span class="punctuation">:</span> <span class="type">timestamp</span> <span class="modifier">auto</span>
   <span class="property">helpful_votes</span><span class="punctuation">:</span> <span class="type">number</span> <span class="modifier">default</span> <span class="number">0</span>
+
+<span class="keyword">interface</span> <span class="interface-name">authentication</span><span class="punctuation">:</span>
+  <span class="ui-keyword">show</span> <span class="ui-element">login_options</span><span class="punctuation">:</span>
+    <span class="property">title</span><span class="punctuation">:</span> <span class="string">"Join the Community"</span>
+    <span class="property">google_login_button</span><span class="punctuation">:</span> <span class="string">"Continue with Google"</span>
+    <span class="property">facebook_login_button</span><span class="punctuation">:</span> <span class="string">"Continue with Facebook"</span>
+    <span class="property">email_signup_link</span><span class="punctuation">:</span> <span class="string">"Sign up with Email"</span>
+    
+  <span class="ui-keyword">show</span> <span class="ui-element">email_registration</span><span class="punctuation">:</span>
+    <span class="property">title</span><span class="punctuation">:</span> <span class="string">"Create Your Account"</span>
+    <span class="property">full_name</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">required</span>
+    <span class="property">email_address</span><span class="punctuation">:</span> <span class="type">email</span> <span class="modifier">required</span>
+    <span class="property">password</span><span class="punctuation">:</span> <span class="type">password</span> <span class="modifier">required</span>
+    <span class="property">create_account_button</span><span class="punctuation">:</span> <span class="string">"Create Account"</span>
 
 <span class="keyword">interface</span> <span class="interface-name">city_discovery</span><span class="punctuation">:</span>
   <span class="ui-keyword">show</span> <span class="ui-element">search_section</span><span class="punctuation">:</span>
     <span class="property">title</span><span class="punctuation">:</span> <span class="string">"Find Your Next City"</span>
     <span class="property">search_input</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">placeholder</span> <span class="string">"Search cities, countries..."</span>
+    <span class="property">search_button</span><span class="punctuation">:</span> <span class="string">"Search"</span>
     
   <span class="ui-keyword">show</span> <span class="ui-element">featured_cities</span><span class="punctuation">:</span>
     <span class="property">title</span><span class="punctuation">:</span> <span class="string">"Popular Destinations"</span>
-    <span class="flow-keyword">for each</span> <span class="variable">city</span> <span class="flow-keyword">in</span> <span class="type">City</span> <span class="flow-keyword">order by</span> <span class="property">average_rating</span> <span class="flow-keyword">desc</span><span class="punctuation">:</span>
+    <span class="flow-keyword">for each</span> <span class="variable">city</span> <span class="flow-keyword">in</span> <span class="type">City</span> <span class="flow-keyword">order by</span> <span class="property">average_rating</span> <span class="flow-keyword">desc limit</span> <span class="number">6</span><span class="punctuation">:</span>
       <span class="ui-keyword">display</span> <span class="ui-element">city_card</span><span class="punctuation">:</span>
         <span class="property">name</span><span class="punctuation">:</span> <span class="variable">city.name</span>
+        <span class="property">country</span><span class="punctuation">:</span> <span class="variable">city.country</span>
         <span class="property">rating</span><span class="punctuation">:</span> <span class="variable">city.average_rating</span> <span class="modifier">stars</span>
         <span class="property">review_count</span><span class="punctuation">:</span> <span class="variable">city.total_reviews</span> <span class="modifier">reviews</span>
+
+<span class="keyword">interface</span> <span class="interface-name">city_details</span><span class="punctuation">:</span>
+  <span class="ui-keyword">show</span> <span class="ui-element">city_header</span><span class="punctuation">:</span>
+    <span class="property">city_name</span><span class="punctuation">:</span> <span class="variable">selected city.name</span>
+    <span class="property">location</span><span class="punctuation">:</span> <span class="variable">city.country, city.state_province</span>
+    <span class="property">overall_rating</span><span class="punctuation">:</span> <span class="variable">city.average_rating</span> <span class="modifier">stars</span>
+    <span class="property">total_reviews</span><span class="punctuation">:</span> <span class="variable">city.total_reviews</span> <span class="modifier">reviews</span>
+    <span class="property">add_review_button</span><span class="punctuation">:</span> <span class="string">"Write a Review"</span>
+    
+  <span class="ui-keyword">show</span> <span class="ui-element">reviews_list</span><span class="punctuation">:</span>
+    <span class="property">title</span><span class="punctuation">:</span> <span class="string">"What People Are Saying"</span>
+    <span class="flow-keyword">for each</span> <span class="variable">review</span> <span class="flow-keyword">in</span> <span class="type">Review</span> <span class="flow-keyword">where</span> <span class="property">city_id</span> <span class="condition">is</span> <span class="variable">selected city</span><span class="punctuation">:</span>
+      <span class="ui-keyword">display</span> <span class="ui-element">review_card</span><span class="punctuation">:</span>
+        <span class="property">reviewer_name</span><span class="punctuation">:</span> <span class="variable">review.user.name</span>
+        <span class="property">rating</span><span class="punctuation">:</span> <span class="variable">review.rating</span> <span class="modifier">stars</span>
+        <span class="property">title</span><span class="punctuation">:</span> <span class="variable">review.title</span>
+        <span class="property">review_text</span><span class="punctuation">:</span> <span class="variable">review.review_text</span>
+        <span class="property">helpful_votes</span><span class="punctuation">:</span> <span class="variable">review.helpful_votes</span> <span class="modifier">helpful</span>
+
+<span class="keyword">interface</span> <span class="interface-name">review_submission</span><span class="punctuation">:</span>
+  <span class="ui-keyword">show</span> <span class="ui-element">review_form</span><span class="punctuation">:</span>
+    <span class="property">title</span><span class="punctuation">:</span> <span class="string">"Share Your Experience"</span>
+    <span class="property">city_info</span><span class="punctuation">:</span> <span class="string">"Reviewing: "</span> <span class="punctuation">+</span> <span class="variable">selected city.name</span>
+    <span class="property">overall_rating</span><span class="punctuation">:</span> <span class="enum">1-5</span> <span class="modifier">stars required</span>
+    <span class="property">review_title</span><span class="punctuation">:</span> <span class="type">text</span> <span class="modifier">required</span>
+    <span class="property">detailed_review</span><span class="punctuation">:</span> <span class="type">textarea</span> <span class="modifier">required</span>
+    <span class="property">lived_or_visited</span><span class="punctuation">:</span> <span class="string">"Your connection to this city:"</span>
+    <span class="property">lived_there</span><span class="punctuation">:</span> <span class="type">boolean</span>
+    <span class="property">visit_duration</span><span class="punctuation">:</span> <span class="enum">days | weeks | months | years</span>
+    <span class="property">submit_button</span><span class="punctuation">:</span> <span class="string">"Submit Review"</span>
+
+<span class="keyword">logic</span> <span class="logic-name">authentication_processing</span><span class="punctuation">:</span>
+  <span class="process-keyword">process</span> <span class="process-name">google_login</span><span class="punctuation">:</span>
+    <span class="flow-keyword">when</span> <span class="variable">user</span> <span class="action">clicks</span> <span class="ui-element">google_login_button</span><span class="punctuation">:</span>
+      <span class="action">authenticate</span> <span class="flow-keyword">with</span> <span class="modifier">google oauth</span>
+      <span class="flow-keyword">if</span> <span class="variable">user</span> <span class="condition">exists</span><span class="punctuation">:</span>
+        <span class="action">create</span> <span class="modifier">user session</span>
+        <span class="flow-keyword">if</span> <span class="property">user.onboarding_completed</span><span class="punctuation">:</span>
+          <span class="action">redirect</span> <span class="flow-keyword">to</span> <span class="ui-element">city_discovery</span>
+        <span class="flow-keyword">else</span><span class="punctuation">:</span>
+          <span class="action">redirect</span> <span class="flow-keyword">to</span> <span class="ui-element">user_onboarding</span>
+      <span class="flow-keyword">else</span><span class="punctuation">:</span>
+        <span class="action">create</span> <span class="type">User</span> <span class="modifier">record with google data</span>
+        <span class="action">redirect</span> <span class="flow-keyword">to</span> <span class="ui-element">user_onboarding</span>
 
 <span class="keyword">logic</span> <span class="logic-name">review_processing</span><span class="punctuation">:</span>
   <span class="process-keyword">process</span> <span class="process-name">submit_review</span><span class="punctuation">:</span>
@@ -74,7 +144,18 @@
         <span class="action">validate</span> <span class="modifier">all required fields</span>
         <span class="action">create</span> <span class="type">Review</span> <span class="modifier">record</span>
         <span class="action">update</span> <span class="property">City.average_rating</span>
+        <span class="action">increment</span> <span class="property">City.total_reviews</span>
+        <span class="action">increment</span> <span class="property">User.total_reviews</span>
+        <span class="action">send</span> <span class="modifier">confirmation message</span>
         <span class="action">redirect</span> <span class="flow-keyword">to</span> <span class="ui-element">city_details</span>
+      <span class="flow-keyword">else</span><span class="punctuation">:</span>
+        <span class="action">redirect</span> <span class="flow-keyword">to</span> <span class="ui-element">authentication</span>
+        
+  <span class="process-keyword">process</span> <span class="process-name">vote_helpful</span><span class="punctuation">:</span>
+    <span class="flow-keyword">when</span> <span class="variable">user</span> <span class="action">clicks</span> <span class="ui-element">helpful_button</span><span class="punctuation">:</span>
+      <span class="flow-keyword">if</span> <span class="variable">user</span> <span class="condition">is authenticated</span><span class="punctuation">:</span>
+        <span class="action">increment</span> <span class="property">Review.helpful_votes</span>
+        <span class="action">update</span> <span class="modifier">review display</span>
       <span class="flow-keyword">else</span><span class="punctuation">:</span>
         <span class="action">redirect</span> <span class="flow-keyword">to</span> <span class="ui-element">authentication</span></code></pre>
             </div>
@@ -86,13 +167,16 @@
           </div>
         </div>
 
-        <!-- Action Buttons -->
+        <!-- LLM Documentation Links -->
         <div class="hero-actions">
-          <a href="/playground" class="btn-primary">
-            🚀 Try APML Playground
+          <a href="/language-spec.txt" class="btn-primary">
+            📋 Full Specification
           </a>
-          <a href="/overview.txt" class="btn-secondary">
-            📖 View Documentation
+          <a href="/business-patterns.txt" class="btn-secondary">
+            💼 Examples
+          </a>
+          <a href="/technical-patterns.txt" class="btn-secondary">
+            ⚙️ Compilation
           </a>
         </div>
 
@@ -230,6 +314,28 @@
 
 .code-input {
   text-align: left;
+  max-height: 500px;
+  overflow-y: auto;
+  scrollbar-width: thin;
+  scrollbar-color: rgba(99, 102, 241, 0.3) rgba(30, 41, 59, 0.3);
+}
+
+.code-input::-webkit-scrollbar {
+  width: 6px;
+}
+
+.code-input::-webkit-scrollbar-track {
+  background: rgba(30, 41, 59, 0.3);
+  border-radius: 3px;
+}
+
+.code-input::-webkit-scrollbar-thumb {
+  background: rgba(99, 102, 241, 0.3);
+  border-radius: 3px;
+}
+
+.code-input::-webkit-scrollbar-thumb:hover {
+  background: rgba(99, 102, 241, 0.5);
 }
 
 .code-input pre {
@@ -237,6 +343,7 @@
   font-family: 'JetBrains Mono', monospace;
   font-size: 0.875rem;
   line-height: 1.5;
+  padding-right: 1rem;
 }
 
 .arrow {
